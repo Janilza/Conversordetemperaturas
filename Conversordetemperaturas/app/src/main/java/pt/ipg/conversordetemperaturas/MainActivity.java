04e62b4fec00942e0e62adb6aec41d9ce9ce782f
Janilza Simão <jasimaio@ua.pt>
2018-10-20 12:03:39 +0100

    bOTÃO

--- Conversordetemperaturas/app/src/main/java/pt/ipg/conversordetemperaturas/MainActivity.java
+++ Conversordetemperaturas/app/src/main/java/pt/ipg/conversordetemperaturas/MainActivity.java
@@ -1,5 +1,6 @@
 package pt.ipg.conversordetemperaturas;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -48,5 +49,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void mostraTemperaturas(View view) {
+        Intent intent = new Intent(this, MostraTemperaturaActivity.class);
+        startActivity(intent);
     }
 }
