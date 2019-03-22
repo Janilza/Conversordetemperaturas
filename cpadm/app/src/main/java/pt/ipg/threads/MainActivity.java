2eeb3ff939ee314bafbc44de46954b64270da4b9
Janilza Simão <jasimaio@ua.pt>
2019-03-22 19:45:34 +0000

    initial

---
+++ cpadm/app/src/main/java/pt/ipg/threads/MainActivity.java
@@ -0,0 +1,22 @@
+package pt.ipg.threads;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void start(View view){
+
+
+    }
+    public void stop(View view){
+
+
+    }
+}
